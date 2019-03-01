55b0cce2f4658f6c5f4d45c96b2f2add268c2150
Donald <donaldcole0@gmail.com>
2019-02-28 19:31:34 -0600

    Completed work on assignment

--- main.py
+++ main.py
@@ -1,70 +1,12 @@
 from flask import Flask, request, redirect, render_template
-import cgi 
-
-app = Flask (__name__)
-app.config['DEBUG']=True
-
-@app.route("/")
-def display_user_signup_form():
-     return render_template("index.html")
-
-@app.route("/", methods=['POST'])
-def validate():
-    #if request.method == 'POST':
-    Username = request.form['username']
-    Email = request.form['email']
-    Password = request.form['password']
-    Verify = request.form['verify'] 
-
-    username_error = ''
-    email_error = ''
-    password_error = ''
-    verify_error = ''
-
-    if len(Username)<3 or len(Username)>20:
-        username_error = "Username is invalid" 
-    if len(Email)<3 or len(Email)>20:
-        email_error = "Email is invalid"
-    if len(Password)<3 or len(Password)>20:
-        password_error = "Password is invalid"   
-        password = ''
-    if password == " " or Verify != password:
-        verify_error = 'Password do not match'
-        verify = ''        
-    
-
-    if username_error or password_error or verify_error or email_error:
-        return render_template('signup.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, email=email)
-        
-    else:
-        return render_template('welcome.html', Username=Username)  
-
-app.run()
-
-"""from flask import Flask, request, redirect, render_template
 import cgi
-#import os
-#import jinja2
-
-#template_dir = os.path.join(os.path.dirname(__file__) ,'templates')
-#jinja.env = jinja2.Evironment (loader = jinja2.FileSystemLoader(template_dir), autoescape=True
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def display_user_signup_form():
-     return render_template('signup.html')
-
-# def username():
-#     if username == '': 
-#         username_error = 'Username must been 3 or more 20'
-#     elif len(username) < 3 or len(username) > 20:
-#         username_error = 'Username is Invaild'
-#     else:
-#          if '' in username:
-#             username_error = 'Username is Invaild'
-#             username = ''
+     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def signup_complete():
@@ -104,14 +46,8 @@ def signup_complete():
             email = ''
     
     if username_error or password_error or verify_error or email_error:
-        return render_template('signup.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, email=email)
+        return render_template('index.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error, email=email)
     else:
         return render_template('welcome.html', username=username)
 
-"""@app.route('/welcome', methods=['GET'])
-"""def welcome():
-    username = request.args.get('username')
-    #template = jinja_env.get_template('welcome.html')
-    return render_template("/welcome.html", username=username)"""
-
-#if __name__ == '__main__':
+app.run()
